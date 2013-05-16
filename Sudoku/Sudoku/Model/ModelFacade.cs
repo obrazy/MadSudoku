6d5d784fb8369550216c224afd3e3df3df88b65d
gj <tite_dent@hotmail.com>
2013-05-16 13:20:02 -0400

    Creation of ModelFacade and restructuration of Solution

---
+++ Sudoku/Sudoku/Model/ModelFacade.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Sudoku.Model
+{
+    public class ModelFacade
+    {
+        #region Properties
+
+        private static ModelFacade _Instance;
+
+        public static ModelFacade Instance
+        {
+            get
+            {
+                if (_Instance == null)
+                {
+                    _Instance = new ModelFacade();
+                }
+                return _Instance;
+            }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        private ModelFacade() { }
+
+        #endregion
+    }
+}
--- Sudoku/Sudoku/ViewModel/SudokuGridViewModel.cs
+++ Sudoku/Sudoku/ViewModel/SudokuGridViewModel.cs
@@ -2,10 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel;
 
 namespace Sudoku.ViewModel
 {
-    class SudokuGridViewModel
+    class SudokuGridViewModel : INotifyPropertyChanged
     {
+        #region Properties
+
+
+
+        #endregion
+
+
     }
 }
